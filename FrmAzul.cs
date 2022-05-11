9a7cf5e5b7217686e8fce16906375e12f96d6dd6
F0nbricio <fabriciorv72@gmail.com>
2022-05-11 14:25:40 -0300

    Criancao do menu

---
+++ FrmAzul.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExemploMenu
+{
+    public partial class FrmAzul : Form
+    {
+        public FrmAzul()
+        {
+            InitializeComponent();
+        }
+
+        private void amareloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exibirFormLaranja_Click(object sender, EventArgs e)
+        {
+            FrmLaranja frm = new FrmLaranja();
+            this.Hide();
+            frm.ShowDialog();
+        }
+
+        private void exibirFormVerde_Click(object sender, EventArgs e)
+        {
+            FrmVerde frm = new FrmVerde();
+            this.Hide();
+            frm.ShowDialog();
+        }
+
+        private void exibirFormAzul_Click(object sender, EventArgs e)
+        {
+            FrmAzul frm = new FrmAzul();//Indico qual forms sera aberto
+            this.Hide();//fecho o atual
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ FrmVerde.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExemploMenu
+{
+    public partial class FrmVerde : Form
+    {
+        public FrmVerde()
+        {
+            InitializeComponent();
+        }
+
+        private void exibirFormAzul_Click(object sender, EventArgs e)
+        {
+            FrmAzul frm = new FrmAzul();
+            this.Hide();
+            frm.ShowDialog();
+        }
+
+        private void exibirFormLaranja_Click(object sender, EventArgs e)
+        {
+            FrmLaranja frm = new FrmLaranja();
+            this.Hide();
+            frm.ShowDialog();
+        }
+
+        private void exibirFormVerde_Click(object sender, EventArgs e)
+        {
+            FrmVerde frm = new FrmVerde();
+            this.Hide();
+            frm.ShowDialog();
+        }
+    }
+}
